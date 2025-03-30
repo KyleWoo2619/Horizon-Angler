c430bbe783a3904fd9c9df58af19bc05eeb9fdd4
KyleWoo2619 <wkhgate2619@gmail.com>
2025-03-29 23:14:53 -0400

    Made strcuture for main menu UI

--- Assets/HorizonAngler_Scripts/MenuController.cs
+++ Assets/HorizonAngler_Scripts/MenuController.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     [Header("Volume Setting")]
     [SerializeField] private TMP_Text volumeTextValue = null;
     [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 0.5f;
 
     [Header("Confirmation")]
     [SerializeField] private GameObject confirmationPrompt = null;
-    
+
     [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
+
     [SerializeField] private GameObject noSavedGameDialog = null;
 
     public void NewGameDialogYes()
@@ -26,7 +29,7 @@ public class MenuController : MonoBehaviour
 
     public void LoadGameDialogYes()
     {
-        if(PlayerPrefs.HasKey("SavedLevel"))
+        if (PlayerPrefs.HasKey("SavedLevel"))
         {
             levelToLoad = PlayerPrefs.GetString("SavedLevel");
             SceneManager.LoadScene(levelToLoad);
@@ -53,6 +56,17 @@ public class MenuController : MonoBehaviour
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
         StartCoroutine(ConfirmationBox());
         //Show Prompt
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
     }
 
     public IEnumerator ConfirmationBox()
